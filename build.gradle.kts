19a9668afd227154257179f61870add00e7b9789
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2020-10-13 22:38:58 +0000

    Update loops-in-progress.md

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
 val ktor_version: String by project
 val kotlin_version: String by project
-val logback_v
+val logback_version: String by project
+val koin_version: String by project
+
+plugins {
+    application
+    ko
