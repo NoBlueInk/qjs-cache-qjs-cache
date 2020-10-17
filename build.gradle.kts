4dceca3c3042fa3cf1c14c37fb0feb737bd39e50
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2020-10-17 20:00:41 +0000

    modified.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,3 +14,6 @@ group = "com.example"
 version = "0.0.1"
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJv
