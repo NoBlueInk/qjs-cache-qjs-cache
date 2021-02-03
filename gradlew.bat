0fece6bfb2c663c107d740d0f70f1ee9245e8606
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-02-03 21:51:38 +0000

    Screenshot updated

--- gradlew.bat
+++ gradlew.bat
@@ -7,4 +7,5 @@
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
-@rem Unless required by applicable law
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed o
