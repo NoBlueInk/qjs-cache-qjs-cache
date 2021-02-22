1f4642e30402f8016ae8a4ce2428f3414ac12163
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-02-22 15:03:09 +0000

    crypto: add missing crypto module aliases

--- gradlew.bat
+++ gradlew.bat
@@ -36,4 +36,8 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
 @rem Find java.exe
-if defined JAVA_HOME goto
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVE
