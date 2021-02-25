ce9e88a0ac8433f362c54f05ce23687004711217
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-02-25 10:07:21 +0000

    All role README files are regenerated

--- gradlew.bat
+++ gradlew.bat
@@ -52,4 +52,9 @@ goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto execute
+
+echo.
+echo ERROR: JAVA_H
