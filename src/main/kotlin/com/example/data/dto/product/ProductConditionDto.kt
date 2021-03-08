5d855aec0b55eb885bc17ec12eb4e6aa4a793498
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-03-08 11:38:18 +0000

    Fixed merge error.

--- src/main/kotlin/com/example/data/dto/product/ProductConditionDto.kt
+++ src/main/kotlin/com/example/data/dto/product/ProductConditionDto.kt
@@ -7,4 +7,5 @@ import kotlinx.serialization.Serializable
 data class ProductConditionDto(
     @SerialName("condition_id") val conditionId: Int,
     @SerialName("condition_name") val conditionName: String,
-    @SerialName("condition_created") val conditionCr
+    @SerialName("condition_created") val conditionCreated: String
+)
