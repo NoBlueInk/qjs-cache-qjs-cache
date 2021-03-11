00c67813b4d3bea78795ce53097efcf97e6293d6
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-03-11 12:13:42 +0000

    fix wrong variable name

--- src/main/kotlin/com/example/data/dto/product/ProductDto.kt
+++ src/main/kotlin/com/example/data/dto/product/ProductDto.kt
@@ -6,4 +6,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class ProductDto(
     @SerialName("product_id") val productId: Long,
-    @SerialName("item_na
+    @SerialName("item_name") val itemName: String,
+    @SerialName("product_code") val productCode: String,
+ 
