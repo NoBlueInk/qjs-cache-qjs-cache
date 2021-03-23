74e445b529dff948b35464a255b9a98e05bdecd7
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-03-23 07:41:45 +0000

    Bump version 0.5.0

--- src/main/kotlin/com/example/data/dto/product/ProductUnitDto.kt
+++ src/main/kotlin/com/example/data/dto/product/ProductUnitDto.kt
@@ -1,3 +1,7 @@
 package com.example.data.dto.product
 
-import kotlinx.serialization.
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ProductUnitDt
