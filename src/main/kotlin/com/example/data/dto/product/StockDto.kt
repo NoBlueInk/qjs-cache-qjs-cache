e8324a5246b9ee7a45b30e996d05abac5faa1174
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-03-29 15:10:58 +0000

    formal changes

--- src/main/kotlin/com/example/data/dto/product/StockDto.kt
+++ src/main/kotlin/com/example/data/dto/product/StockDto.kt
@@ -1,4 +1,8 @@
 package com.example.data.dto.product
 
 import kotlinx.serialization.SerialName
-import kotlinx.se
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class StockDto(
+    @SerialName("stock_id") val stockId: 
