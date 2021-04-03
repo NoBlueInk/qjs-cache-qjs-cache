361b9ee4b1acb235397c495e72441e16f1d68e61
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-04-03 08:52:08 +0000

    change interface

--- src/main/kotlin/com/example/data/dto/product/StockWeightDto.kt
+++ src/main/kotlin/com/example/data/dto/product/StockWeightDto.kt
@@ -3,3 +3,7 @@ package com.example.data.dto.product
 import com.example.data.entities.productsEntites.StockEntity
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+
+@Serializable
+data class StockWeightDto(
+    @SerialName("stock_weight_id") val stockWeightId: Int,
