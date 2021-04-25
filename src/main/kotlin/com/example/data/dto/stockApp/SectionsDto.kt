e95382367e01b41d028b1f9f34bdb1e1a4a42bef
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-04-25 22:20:11 +0000

    kmemleak: free internal objects only if there're no leaks to be reported

--- src/main/kotlin/com/example/data/dto/stockApp/SectionsDto.kt
+++ src/main/kotlin/com/example/data/dto/stockApp/SectionsDto.kt
@@ -5,4 +5,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SectionsDto(
-    @Seria
+    @SerialName("sections_id") val sectionsID: Int,
+    @SerialName("section_name") val
