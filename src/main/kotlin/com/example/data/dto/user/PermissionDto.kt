8a3f50ec88d7d27a207f13fa819e5dc1b8541ce8
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-04-29 09:23:10 +0000

    Added groups

--- src/main/kotlin/com/example/data/dto/user/PermissionDto.kt
+++ src/main/kotlin/com/example/data/dto/user/PermissionDto.kt
@@ -2,3 +2,7 @@ package com.example.data.dto.user
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+
+@Serializable
+data class PermissionDto(
+    @SerialName("permission_id") val permiss
