1fcc6086bb08289d00e001ac985507b3d0f9c596
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-05-11 15:23:06 +0000

    Remove unnecessary check

--- src/main/kotlin/com/example/data/entities/authEntites/RolesPermissionsEntity.kt
+++ src/main/kotlin/com/example/data/entities/authEntites/RolesPermissionsEntity.kt
@@ -3,4 +3,6 @@ package com.example.data.entities.authEntites
 import org.ktorm.entity.Entity
 
 interface RolesPermissionsEntity : Entity<RolesPermissionsEntity> {
-    companion o
+    companion object : Entity.Factory<RolesPermissionsEntity>()
+
+    val role: RoleEnt
