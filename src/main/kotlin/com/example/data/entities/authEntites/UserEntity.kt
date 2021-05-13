131bb4126e55af865b1614594eba99aea88ce649
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-05-13 09:30:37 +0000

    Adjust style

--- src/main/kotlin/com/example/data/entities/authEntites/UserEntity.kt
+++ src/main/kotlin/com/example/data/entities/authEntites/UserEntity.kt
@@ -4,4 +4,9 @@ import org.ktorm.entity.Entity
 import java.time.LocalDateTime
 
 interface UserEntity : Entity<UserEntity> {
-    companion object : Entity.Factory<UserEntity>(
+    companion object : Entity.Factory<UserEntity>()
+
+    var userId: Int
+    var userRole: Int
+    var username: String
+    var user
