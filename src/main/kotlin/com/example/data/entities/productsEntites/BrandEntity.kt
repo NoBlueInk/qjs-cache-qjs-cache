3cc8869905f4cd3c9ae32faef34f349518cde354
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-05-25 06:26:08 +0000

    project file changes

--- src/main/kotlin/com/example/data/entities/productsEntites/BrandEntity.kt
+++ src/main/kotlin/com/example/data/entities/productsEntites/BrandEntity.kt
@@ -1,4 +1,8 @@
 package com.example.data.entities.productsEntites
 
 import org.ktorm.entity.Entity
-import java.
+import java.time.Instant
+import java.time.LocalDateTime
+
+interface BrandEntity : Entity<BrandEntity> {
+   
