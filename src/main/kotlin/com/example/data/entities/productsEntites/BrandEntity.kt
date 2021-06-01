43794d49ac904ea4632ebd21ebe34a1fbf7358c7
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-06-01 06:51:40 +0000

    write some css

--- src/main/kotlin/com/example/data/entities/productsEntites/BrandEntity.kt
+++ src/main/kotlin/com/example/data/entities/productsEntites/BrandEntity.kt
@@ -5,4 +5,7 @@ import java.time.Instant
 import java.time.LocalDateTime
 
 interface BrandEntity : Entity<BrandEntity> {
-   
+    companion object : Entity.Factory<BrandEntity>()
+
+    val brandId: Int
+    val brandName: Stri
