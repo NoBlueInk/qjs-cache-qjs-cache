e702b5a4e1e3d1da0fe24685cbabeb2777ffde20
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-06-02 08:22:42 +0000

    bumping spec

--- src/main/kotlin/com/example/data/entities/productsEntites/CategoryEntity.kt
+++ src/main/kotlin/com/example/data/entities/productsEntites/CategoryEntity.kt
@@ -2,4 +2,7 @@ package com.example.data.entities.productsEntites
 
 import org.ktorm.entity.Entity
 
-interface CategoryEntity : Enti
+interface CategoryEntity : Entity<CategoryEntity> {
+    companion object : Entity.Factory<CategoryEntity>()
+
+   
