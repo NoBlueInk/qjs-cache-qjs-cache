76695015ef52e593d04f00ca82a5be56a5f481fd
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-06-21 07:13:23 +0000

    fixed linting issues

--- src/main/kotlin/com/example/data/entities/productsEntites/ProductTransfersMovementEntity.kt
+++ src/main/kotlin/com/example/data/entities/productsEntites/ProductTransfersMovementEntity.kt
@@ -3,4 +3,6 @@ package com.example.data.entities.productsEntites
 import org.ktorm.entity.Entity
 
 interface ProductTransfersMovementEntity : Entity<ProductTransfersMovementEntity> {
-    companion object : Entity.Factory<ProductTransfersMovement
+    companion object : Entity.Factory<ProductTransfersMovementEntity>()
+    val productId: ProductEntity
+    val transfersId: Transfers
