f642ea4951366a42149b983d719d2789de21cec7
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-06-27 07:50:43 +0000

    v0.2.0 dev

--- src/main/kotlin/com/example/data/entities/productsEntites/WarehousesEntity.kt
+++ src/main/kotlin/com/example/data/entities/productsEntites/WarehousesEntity.kt
@@ -2,4 +2,8 @@ package com.example.data.entities.productsEntites
 
 import org.ktorm.entity.Entity
 
-interface WarehousesEntity : Entity<Wareh
+interface WarehousesEntity : Entity<WarehousesEntity> {
+    companion object : Entity.Factory<WarehousesEntity>()
+
+    val warehouseId: Int
+    
