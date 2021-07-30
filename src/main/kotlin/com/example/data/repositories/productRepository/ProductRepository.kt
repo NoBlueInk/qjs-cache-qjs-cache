091ac5f763f51fc0915adb6786e05578777c148b
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-07-30 21:43:01 +0000

    Moving files

--- src/main/kotlin/com/example/data/repositories/productRepository/ProductRepository.kt
+++ src/main/kotlin/com/example/data/repositories/productRepository/ProductRepository.kt
@@ -3,4 +3,8 @@ package com.example.data.repositories.productRepository
 import com.example.data.dto.product.*
 import com.example.data.entities.productsEntites.ProductTransfersMovementEntity
 
-interface Product
+interface ProductRepository {
+
+    suspend fun getAllProductsFromStocks(): List<StockDto>
+
+    suspen
