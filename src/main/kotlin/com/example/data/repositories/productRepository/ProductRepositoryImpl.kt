897f4f1e9024187c6aadeae3ad1df5579a220229
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-10-01 06:02:08 +0000

    Fixed redirect

--- src/main/kotlin/com/example/data/repositories/productRepository/ProductRepositoryImpl.kt
+++ src/main/kotlin/com/example/data/repositories/productRepository/ProductRepositoryImpl.kt
@@ -33,3 +33,68 @@ class ProductRepositoryImpl : ProductRepository {
 //        return dataBase.supplier.map { it.toSupplerDto() }
         TODO("Not yet implemented")
     }
+
+    override suspend fun getAllBrands(): List<BrandDto> {
+//        return dataBase.brands.map { it.toBrandDto() }
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllTransfers(): List<TransfersDto> {
+//        return dataBase.transfers.map { it.toTransfersDto() }
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllWarehouses(): List<WarehousesDto> {
+//        return dataBase.warehouses.map { it.toWarehousesDto() }
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllTransfersType(): List<TransfersTypeDto> {
+//        return dataBase.transfersType.map { it.toTransfersTypeDto() }
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllProductTransfersMovement(): List<ProductTransfersMovementDto> {
+//        return dataBase.productTransferMovements.map { it.toTransferMovementDto() }
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllStockWeight(): List<StockWeightDto> {
+//        return dataBase.stockWeight.map { it.toStockWeightDto() }
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllProductUnits(): List<ProductUnitDto> {
+//        return dataBase.productUnit.map { it.toProductUnitDto() }
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllStockColors(): List<StockColorDto> {
+//        return dataBase.stockColor.map { it.toStockColorDto() }
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllProductCondition(): List<ProductConditionDto> {
+//        return dataBase.productCondition.map { it.toProductConditionDto() }
+        TODO("Not yet implemented")
+    }
+
+    /*
+    SELECT stock_id, stock.product_id, item_name,
+    product.product_code, product_created, quantity_in_hand,
+    item_image, item_price, brand_name, supplier_name,
+    unit_name, warehouse_name
+    FROM product, stock, product_unit, warehouses, brand, supplier
+    where product.product_id = stock.product_id
+    and product_unit.unit_id = product.unit_id
+    and brand.brand_id = product.brand_id
+    and supplier.supplier_id = product.supplier_id
+    and warehouses.warehouse_id = stock.warehouse_id
+    order by product.product_id
+    */
+
+    override suspend fun getCustom(): List<ProductDto> {
+        TODO()
+    }
+
+}
