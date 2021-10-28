51da35e6ca7174ba684641513480a979857f7d00
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2021-10-28 12:32:10 +0000

    Add locales

--- src/main/kotlin/com/example/data/repositories/stockRepository/StockAppRepositoryImpl.kt
+++ src/main/kotlin/com/example/data/repositories/stockRepository/StockAppRepositoryImpl.kt
@@ -5,3 +5,10 @@ import com.example.data.dto.stockApp.SectionsDto
 import com.example.db.sectionApp
 import com.example.domin.mapper.stockappMapper.toSectionDto
 import org.ktorm.entity.map
+
+class StockAppRepositoryImpl : StockAppRepository {
+    override suspend fun getSectionsStockApp(): List<SectionsDto> {
+//        return dataBase.sectionApp.map { it.toSectionDto() }
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/kotlin/com/example/data/repositories/userRepository/UserRepository.kt
@@ -0,0 +1,3 @@
+package com.example.data.repositories.userRepository
+
+import com.ex
