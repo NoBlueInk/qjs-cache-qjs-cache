1041ba6f992d1a2bd56b092287a9035c51f13d34
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-02-25 21:20:59 +0000

    xfs: Fix possible use-after-free with AIO

--- src/main/kotlin/com/example/data/tables/authTables/PermissionsTable.kt
+++ src/main/kotlin/com/example/data/tables/authTables/PermissionsTable.kt
@@ -1,4 +1,8 @@
 package com.example.data.tables.authTables
 
 import com.example.data.entities.authEntites.PermissionEntity
-import org.ktorm.sc
+import org.ktorm.schema.Table
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
+
+object Permissi
