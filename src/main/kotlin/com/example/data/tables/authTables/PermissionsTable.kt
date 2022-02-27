70d4a7ac8ecfdea2fb358531e66782f28cd28043
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-02-27 14:13:22 +0000

    PEP8 Fixes

--- src/main/kotlin/com/example/data/tables/authTables/PermissionsTable.kt
+++ src/main/kotlin/com/example/data/tables/authTables/PermissionsTable.kt
@@ -5,4 +5,5 @@ import org.ktorm.schema.Table
 import org.ktorm.schema.int
 import org.ktorm.schema.varchar
 
-object Permissi
+object PermissionsTable : Table<PermissionEntity>("permissions") {
+    val permissionId = int("permission_id").bindTo 
