3321f8652602a19a21b7ebd6dd472944021f6085
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-02-28 12:44:44 +0000

    Fixed some tests.

--- src/main/kotlin/com/example/data/tables/authTables/RolesPermissionsTable.kt
+++ src/main/kotlin/com/example/data/tables/authTables/RolesPermissionsTable.kt
@@ -1,3 +1,5 @@
 package com.example.data.tables.authTables
 
-import com.example.data.entities.authEntites.Roles
+import com.example.data.entities.authEntites.RolesPermissionsEntity
+import org.ktorm.schema.Table
+import org.ktorm.schem
