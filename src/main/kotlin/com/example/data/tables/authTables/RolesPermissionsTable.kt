dc631838f0e22efc1a096f6dd6cd033672b0988f
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-02-28 16:22:54 +0000

    Update revengerat.txt

--- src/main/kotlin/com/example/data/tables/authTables/RolesPermissionsTable.kt
+++ src/main/kotlin/com/example/data/tables/authTables/RolesPermissionsTable.kt
@@ -2,4 +2,7 @@ package com.example.data.tables.authTables
 
 import com.example.data.entities.authEntites.RolesPermissionsEntity
 import org.ktorm.schema.Table
-import org.ktorm.schem
+import org.ktorm.schema.int
+
+object RolesPermissionsTable : Table<RolesPermissionsEntity>("rolespermissions") {
+ 
