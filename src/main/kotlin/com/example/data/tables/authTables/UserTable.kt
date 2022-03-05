1d22ba150c723391bc0dc5395b759bbddd084395
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-03-05 08:56:02 +0000

    updated text

--- src/main/kotlin/com/example/data/tables/authTables/UserTable.kt
+++ src/main/kotlin/com/example/data/tables/authTables/UserTable.kt
@@ -1,3 +1,5 @@
 package com.example.data.tables.authTables
 
-import com.example.data.entities.authEntites.Us
+import com.example.data.entities.authEntites.UserEntity
+import com.example.data.tables.productsTabels.ProductTable.bindTo
+import kotlinx.
