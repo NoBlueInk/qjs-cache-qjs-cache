7e6b04a4920432615761aafb3895dc71f05826c5
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-03-05 12:42:48 +0000

    image resizing

--- src/main/kotlin/com/example/data/tables/authTables/UserTable.kt
+++ src/main/kotlin/com/example/data/tables/authTables/UserTable.kt
@@ -2,4 +2,6 @@ package com.example.data.tables.authTables
 
 import com.example.data.entities.authEntites.UserEntity
 import com.example.data.tables.productsTabels.ProductTable.bindTo
-import kotlinx.
+import kotlinx.serialization.SerialName
+import org.ktorm.schema.Table
+import org.ktorm.schema.dat
