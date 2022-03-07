482ab623fae8f154d299f189fdb0f8ee5f34ecaa
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-03-07 16:14:32 +0000

    logo update

--- src/main/kotlin/com/example/data/tables/authTables/UserTable.kt
+++ src/main/kotlin/com/example/data/tables/authTables/UserTable.kt
@@ -4,4 +4,8 @@ import com.example.data.entities.authEntites.UserEntity
 import com.example.data.tables.productsTabels.ProductTable.bindTo
 import kotlinx.serialization.SerialName
 import org.ktorm.schema.Table
-import org.ktorm.schema.dat
+import org.ktorm.schema.datetime
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
+
+object User
