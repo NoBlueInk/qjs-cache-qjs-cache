16d0422c69bc721059756e2158cf638279723711
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-03-13 09:37:13 +0000

    Bump version to 0.6.1

--- src/main/kotlin/com/example/data/tables/productsTabels/BrandTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/BrandTable.kt
@@ -1,3 +1,7 @@
 package com.example.data.tables.productsTabels
 
-import com.example.data.entities.productsEntites.BrandEnt
+import com.example.data.entities.productsEntites.BrandEntity
+import org.ktorm.schema.*
+
+object BrandTable : Table<BrandEntity>("brand") {
+    val b
