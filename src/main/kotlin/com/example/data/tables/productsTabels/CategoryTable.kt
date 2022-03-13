c0b4bfe6ff13b7f4e0148a6986efc4c4b750b0a5
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-03-13 20:27:56 +0000

    Remove unused template

--- src/main/kotlin/com/example/data/tables/productsTabels/CategoryTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/CategoryTable.kt
@@ -4,3 +4,6 @@ import com.example.data.entities.productsEntites.CategoryEntity
 import org.ktorm.schema.Table
 import org.ktorm.schema.int
 import org.ktorm.schema.varchar
+
+object CategoryTable: Table<CategoryEntity>("category") {
+    val categoryId = int("catego
