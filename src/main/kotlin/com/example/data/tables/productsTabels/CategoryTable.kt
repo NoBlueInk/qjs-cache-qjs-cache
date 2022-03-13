32fde61f950a9aea7c33998929ee697ac987588c
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-03-13 19:55:34 +0000

    Renamed README file

--- src/main/kotlin/com/example/data/tables/productsTabels/CategoryTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/CategoryTable.kt
@@ -1,4 +1,6 @@
 package com.example.data.tables.productsTabels
 
 import com.example.data.entities.productsEntites.CategoryEntity
-imp
+import org.ktorm.schema.Table
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
