7e77254dcae09ddaa493f3fd96c1093acd739647
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-03-16 10:16:31 +0000

    sdsdsd

--- src/main/kotlin/com/example/data/tables/productsTabels/ColorTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/ColorTable.kt
@@ -2,3 +2,7 @@ package com.example.data.tables.productsTabels
 
 import com.example.data.entities.productsEntites.ColorEntity
 import org.ktorm.schema.Table
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
+
+object ColorTable: Table<ColorEnti
