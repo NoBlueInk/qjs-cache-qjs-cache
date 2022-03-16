1f6171e967db599d1c3808796686c4c670202937
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-03-16 21:19:29 +0000

    Automatic merge of Transifex translations

--- src/main/kotlin/com/example/data/tables/productsTabels/ColorTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/ColorTable.kt
@@ -5,4 +5,5 @@ import org.ktorm.schema.Table
 import org.ktorm.schema.int
 import org.ktorm.schema.varchar
 
-object ColorTable: Table<ColorEnti
+object ColorTable: Table<ColorEntity>("color") {
+    val colorId = int("color_id").bindTo { it.colorId
