cfa39aff789a028785bb3090f07556061c61f404
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-03-24 08:03:58 +0000

    Fix #50

--- src/main/kotlin/com/example/data/tables/productsTabels/ProductTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/ProductTable.kt
@@ -1,3 +1,5 @@
 package com.example.data.tables.productsTabels
 
-import com.example.data.entities.productsEntites.Prod
+import com.example.data.entities.productsEntites.ProductEntity
+import com.example.data.tables.productsTabels.ProductConditionTable.bindTo
+import o
