4e7bdf9798bb6d0b57513260d3c554f83b0ef098
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-03-24 13:22:53 +0000

    update project.clj

--- src/main/kotlin/com/example/data/tables/productsTabels/ProductTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/ProductTable.kt
@@ -2,4 +2,7 @@ package com.example.data.tables.productsTabels
 
 import com.example.data.entities.productsEntites.ProductEntity
 import com.example.data.tables.productsTabels.ProductConditionTable.bindTo
-import o
+import org.ktorm.schema.*
+
+object ProductTable : Table<ProductEntity>("product") {
+    val productId = lon
