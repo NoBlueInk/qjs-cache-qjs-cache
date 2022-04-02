76430570bec512da511023e0fe86103ba60e79e7
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-04-02 10:34:28 +0000

    msm: mdss: fix race condition between shutdown thread and fb ioctl

--- src/main/kotlin/com/example/data/tables/productsTabels/StockColorTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/StockColorTable.kt
@@ -1,4 +1,7 @@
 package com.example.data.tables.productsTabels
 
 import com.example.data.entities.productsEntites.StockColorEntity
-import com.example.data.tables.authTables
+import com.example.data.tables.authTables.PermissionsTable.references
+import org.ktorm.schema.Table
+import org.ktorm.schema.float
+i
