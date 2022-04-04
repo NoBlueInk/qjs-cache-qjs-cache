1b5b2e6cb6814bff134e94a77d47210d6feb25ed
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-04-04 22:23:41 +0000

    Initial commmit

--- src/main/kotlin/com/example/data/tables/productsTabels/StockTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/StockTable.kt
@@ -1,4 +1,7 @@
 package com.example.data.tables.productsTabels
 
 import com.example.data.entities.productsEntites.StockEntity
-import com.example.data.tables.productsTabels.
+import com.example.data.tables.productsTabels.TransfersTypeTable.bindTo
+import org.ktorm.schema.*
+
+object StockTable : Table<StockEntity>
