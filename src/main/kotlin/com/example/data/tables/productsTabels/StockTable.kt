3bbefc3490a8ba418bff88630486b5d3bf4a572c
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-04-05 21:13:29 +0000

    prepare for merge

--- src/main/kotlin/com/example/data/tables/productsTabels/StockTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/StockTable.kt
@@ -4,4 +4,6 @@ import com.example.data.entities.productsEntites.StockEntity
 import com.example.data.tables.productsTabels.TransfersTypeTable.bindTo
 import org.ktorm.schema.*
 
-object StockTable : Table<StockEntity>
+object StockTable : Table<StockEntity>("stock") {
+    val stockId = long("stock_id").bindTo { it.stockId }.primaryKey()
+    val produc
