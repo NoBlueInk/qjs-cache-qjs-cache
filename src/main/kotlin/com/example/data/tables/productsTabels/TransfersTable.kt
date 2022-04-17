65f17c0758472b3a8bc2fab42c733aa98e8ac47b
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-04-17 17:51:34 +0000

    clarified comments

--- src/main/kotlin/com/example/data/tables/productsTabels/TransfersTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/TransfersTable.kt
@@ -3,4 +3,6 @@ package com.example.data.tables.productsTabels
 import com.example.data.entities.productsEntites.TransfersEntity
 import org.ktorm.schema.*
 
-object TransfersTable : Table<TransfersEntity
+object TransfersTable : Table<TransfersEntity>("transfers"){
+    val transferId = int("transfer_id").bindTo { it.transferId }.primaryKey()
+    val stockCondi
