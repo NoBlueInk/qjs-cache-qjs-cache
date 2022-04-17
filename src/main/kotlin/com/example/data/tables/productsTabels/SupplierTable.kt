c93668c987bc77b06d2c73f631dd5e83103ddb8d
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-04-17 07:40:13 +0000

    update indigo

--- src/main/kotlin/com/example/data/tables/productsTabels/SupplierTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/SupplierTable.kt
@@ -2,3 +2,6 @@ package com.example.data.tables.productsTabels
 
 import com.example.data.entities.productsEntites.SupplierEntity
 import org.ktorm.schema.*
+
+object SupplierTable: Table<SupplierEntity>("supplier") {
+    val supplierId 
