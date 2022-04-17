7ea533e7119e8b61ad6adb73d88e05c0bc66135e
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-04-17 15:08:14 +0000

    fix setting

--- src/main/kotlin/com/example/data/tables/productsTabels/TransfersTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/TransfersTable.kt
@@ -1,3 +1,6 @@
 package com.example.data.tables.productsTabels
 
-import com.example.data.entities.productsEntites
+import com.example.data.entities.productsEntites.TransfersEntity
+import org.ktorm.schema.*
+
+object TransfersTable : Table<TransfersEntity
