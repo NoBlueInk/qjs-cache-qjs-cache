6cb471c8891a1baec18b3e76d3b3594d82703a28
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-04-19 11:14:30 +0000

    removed unnecessary require

--- src/main/kotlin/com/example/data/tables/productsTabels/TransfersTypeTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/TransfersTypeTable.kt
@@ -1,3 +1,4 @@
 package com.example.data.tables.productsTabels
 
-import com.
+import com.example.data.entities.productsEntites.TransfersTypeEntity
+import com.example.data.tables.productsTabels.Ware
