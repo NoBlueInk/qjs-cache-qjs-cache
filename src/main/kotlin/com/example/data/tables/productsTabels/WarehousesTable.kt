f2527d62a3cc229691c30a6c58a260de3bc5e9c8
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-04-22 06:44:28 +0000

    doc clarification

--- src/main/kotlin/com/example/data/tables/productsTabels/WarehousesTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/WarehousesTable.kt
@@ -2,4 +2,7 @@ package com.example.data.tables.productsTabels
 
 import com.example.data.entities.productsEntites.WarehousesEntity
 import org.ktorm.schema.Table
-import org.
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
+
+object WarehousesTa
