96cf79f97b7ffb7d09a356d73cf4de43656466dc
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-04-22 14:06:35 +0000

    Renamed directory

--- src/main/kotlin/com/example/data/tables/productsTabels/WarehousesTable.kt
+++ src/main/kotlin/com/example/data/tables/productsTabels/WarehousesTable.kt
@@ -5,4 +5,5 @@ import org.ktorm.schema.Table
 import org.ktorm.schema.int
 import org.ktorm.schema.varchar
 
-object WarehousesTa
+object WarehousesTable: Table<WarehousesEntity>("warehouses") {
+    val warehouseId = int("wareho
