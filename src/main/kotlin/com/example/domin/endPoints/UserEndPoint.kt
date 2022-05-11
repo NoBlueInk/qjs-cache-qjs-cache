867b320685b314fd4429651fe62d21998e03c587
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-05-11 12:42:45 +0000

    Script update.

--- src/main/kotlin/com/example/domin/endPoints/UserEndPoint.kt
+++ src/main/kotlin/com/example/domin/endPoints/UserEndPoint.kt
@@ -4,4 +4,6 @@ sealed class UserEndPoint(val path:String){
     object Root: UserEndPoint(path = "/")
     object SignUp: UserEndPoint(path = "user/add_user")
     object SignIn: UserEndPoint(path = "user/sign_in")
-    object Authenticate: UserEndPoint
+    object Authenticate: UserEndPoint(path = "user/authenticate")
+    object Secret: UserEndPoint(path = "user/secret")
+    object
