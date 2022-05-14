749e89cad56fcc9673d8b049e151999e7c002451
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-05-14 20:15:33 +0000

    Typo corrected.

--- src/main/kotlin/com/example/domin/mapper/BrandMapper.kt
+++ src/main/kotlin/com/example/domin/mapper/BrandMapper.kt
@@ -2,3 +2,7 @@ package com.example.domin.mapper
 
 import com.example.data.dto.product.BrandDto
 import com.example.data.entities.productsEntites.BrandEntity
+
+fun BrandEntity.toBrandDto() = BrandDto(
+    brandId = brandId,
+    brand
