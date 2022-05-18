446fa056831a632f894ba9de14627a8e3345d407
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-05-18 19:37:02 +0000

    some experiments

--- src/main/kotlin/com/example/domin/mapper/ColorMapper.kt
+++ src/main/kotlin/com/example/domin/mapper/ColorMapper.kt
@@ -1,4 +1,6 @@
 package com.example.domin.mapper
 
 import com.example.data.dto.product.ColorDto
-import com
+import com.example.data.entities.productsEntites.ColorEntity
+
+fun ColorEntity.toColorDto
