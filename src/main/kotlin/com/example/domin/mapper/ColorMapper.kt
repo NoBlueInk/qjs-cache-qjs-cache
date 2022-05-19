ff33585bc8d40aa3ebfd1e9fed0fa9d12e2b8995
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-05-19 08:39:34 +0000

    dm thin: fix dangling bio in process_deferred_bios error path

--- src/main/kotlin/com/example/domin/mapper/ColorMapper.kt
+++ src/main/kotlin/com/example/domin/mapper/ColorMapper.kt
@@ -3,4 +3,7 @@ package com.example.domin.mapper
 import com.example.data.dto.product.ColorDto
 import com.example.data.entities.productsEntites.ColorEntity
 
-fun ColorEntity.toColorDto
+fun ColorEntity.toColorDto() = ColorDto(
+    colorId = colorId,
+    colorName = colorName,
+    colorHe
