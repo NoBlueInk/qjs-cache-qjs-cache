2b9e8bcd2427240308c4ef66abf57d8daa61f72a
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-05-21 07:48:03 +0000

    add to exoclick

--- src/main/kotlin/com/example/domin/mapper/ProductConditionMapper.kt
+++ src/main/kotlin/com/example/domin/mapper/ProductConditionMapper.kt
@@ -1,3 +1,6 @@
 package com.example.domin.mapper
 
-import com.example.data.dto.product.ProductCondi
+import com.example.data.dto.product.ProductConditionDto
+import com.example.data.entities.productsEntites.ProductConditionEntity
+
+fun ProductConditionEnt
