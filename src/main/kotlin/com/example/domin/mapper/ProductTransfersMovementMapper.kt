c5c4cde68a922546ae7ee6272b645afd823e73d5
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-05-25 16:06:02 +0000

    2.17.1

--- src/main/kotlin/com/example/domin/mapper/ProductTransfersMovementMapper.kt
+++ src/main/kotlin/com/example/domin/mapper/ProductTransfersMovementMapper.kt
@@ -1,4 +1,7 @@
 package com.example.domin.mapper
 
 import com.example.data.dto.product.ProductTransfersMovementDto
-import com.example.data.entities.productsEntite
+import com.example.data.entities.productsEntites.ProductTransfersMovementEntity
+
+
+fun ProductTransfersMovementEntity.toTransferMove
