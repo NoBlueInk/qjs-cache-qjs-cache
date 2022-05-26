183072bedc8a687a3953731da2e166835d82bd04
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-05-26 21:54:18 +0000

    Fix configuration

--- src/main/kotlin/com/example/domin/mapper/StockMapper.kt
+++ src/main/kotlin/com/example/domin/mapper/StockMapper.kt
@@ -1,4 +1,8 @@
 package com.example.domin.mapper
 
 import com.example.data.dto.product.StockDto
-import com.example.data.entities.product
+import com.example.data.entities.productsEntites.StockEntity
+
+fun StockEntity.toStockDto() = StockDto(
+    stockId = stockId,
+ 
