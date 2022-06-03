6b12082b4d0d09b06060a6e9e0efc707986a8594
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-06-03 23:07:16 +0000

    Updating README.md file.

--- src/main/kotlin/com/example/domin/mapper/StockWeightMapper.kt
+++ src/main/kotlin/com/example/domin/mapper/StockWeightMapper.kt
@@ -1,4 +1,7 @@
 package com.example.domin.mapper
 
 import com.example.data.dto.product.StockWeightDto
-import com.example.
+import com.example.data.entities.productsEntites.StockWeightEntity
+
+fun StockWeightEntity.toStockWeightDto() =
+    
