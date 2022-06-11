3c7935537c90d339fe09e239aacbce177048298e
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-06-11 22:09:58 +0000

    add new method

--- src/main/kotlin/com/example/domin/mapper/StockWeightMapper.kt
+++ src/main/kotlin/com/example/domin/mapper/StockWeightMapper.kt
@@ -4,4 +4,6 @@ import com.example.data.dto.product.StockWeightDto
 import com.example.data.entities.productsEntites.StockWeightEntity
 
 fun StockWeightEntity.toStockWeightDto() =
-    
+    StockWeightDto(
+        stockWeightId = stockWeightId,
+        stockId =
