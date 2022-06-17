13e801546b039ebcc6653054e4c4853bbd5a1122
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-06-17 19:42:09 +0000

    Update translations.php

--- src/main/kotlin/com/example/domin/mapper/TransfersMapper.kt
+++ src/main/kotlin/com/example/domin/mapper/TransfersMapper.kt
@@ -3,4 +3,6 @@ package com.example.domin.mapper
 import com.example.data.dto.product.TransfersDto
 import com.example.data.entities.productsEntites.TransfersEntity
 
-fun TransfersEntity.toTransfersDto
+fun TransfersEntity.toTransfersDto() = TransfersDto(
+    transferId = transferId,
+    stockConditionId = stockCon
