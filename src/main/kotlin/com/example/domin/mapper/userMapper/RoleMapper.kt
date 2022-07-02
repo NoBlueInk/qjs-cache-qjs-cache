293a7ee05594d5146f2da19bc96904888f6a24c6
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-07-02 08:36:42 +0000

    update config files

--- src/main/kotlin/com/example/domin/mapper/userMapper/RoleMapper.kt
+++ src/main/kotlin/com/example/domin/mapper/userMapper/RoleMapper.kt
@@ -1,4 +1,9 @@
 package com.example.domin.mapper.userMapper
 
 import com.example.data.dto.user.RoleDto
-import com.example.data.entitie
+import com.example.data.entities.authEntites.RoleEntity
+
+fun RoleEntity.toRoleDto() =
+    RoleDto(
+        rolesId = rolesId,
+        
