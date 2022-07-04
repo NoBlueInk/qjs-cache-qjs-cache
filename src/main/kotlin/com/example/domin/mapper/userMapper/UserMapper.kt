f791c93c9c07e3e8a512bceedb7014328394f43a
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-07-04 13:18:08 +0000

    to

--- src/main/kotlin/com/example/domin/mapper/userMapper/UserMapper.kt
+++ src/main/kotlin/com/example/domin/mapper/userMapper/UserMapper.kt
@@ -1,4 +1,7 @@
 package com.example.domin.mapper.userMapper
 
 import com.example.data.dto.user.UserDto
-import com.example.data.ent
+import com.example.data.entities.authEntites.UserEntity
+
+fun UserEntity.toUserDto() = UserDto(
+    us
