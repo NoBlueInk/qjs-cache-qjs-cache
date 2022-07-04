9ba1e69a4c80de043c0e02b35d7e9bddb01f72a9
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-07-04 08:06:01 +0000

    netifd: update to latest git HEAD

--- src/main/kotlin/com/example/domin/mapper/userMapper/RolePermissionMapper.kt
+++ src/main/kotlin/com/example/domin/mapper/userMapper/RolePermissionMapper.kt
@@ -1,4 +1,6 @@
 package com.example.domin.mapper.userMapper
 
 import com.example.data.dto.user.RolesPermissionsDto
-import com.e
+import com.example.data.entities.authEntites.RolesPermissionsEntity
+
+fun RolesPermissionsEntity.to
