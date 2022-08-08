5342b9c5db4a9150692d724bdf6fca28b6cffa79
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-08-08 07:44:24 +0000

    Run update-remote.sh on wordpress

--- src/main/kotlin/com/example/plugins/routing/ProductsRouting.kt
+++ src/main/kotlin/com/example/plugins/routing/ProductsRouting.kt
@@ -5,4 +5,7 @@ import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
 fun Application.productsConfigRouting() {
-   
+    routing {
+        getAllBrandsRoute()
+        getAllCategoryRoute()
+        getAll
