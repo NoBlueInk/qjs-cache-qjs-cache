71991f06750bbaa0c54d77f4e7f22bd1ab136bbe
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-08-26 07:34:15 +0000

    Update TESTING.md

--- src/main/kotlin/com/example/routes/mapper/signInMapper/SignInParameterMapper.kt
+++ src/main/kotlin/com/example/routes/mapper/signInMapper/SignInParameterMapper.kt
@@ -1,4 +1,8 @@
 package com.example.routes.mapper.signInMapper
 
 import com.example.data.request.auth.UserSignInRequest
-import io.ktor.server.application
+import io.ktor.server.application.*
+import io.ktor.server.util.*
+import io.ktor.util.pipeline.*
+
+fun Pipeli
