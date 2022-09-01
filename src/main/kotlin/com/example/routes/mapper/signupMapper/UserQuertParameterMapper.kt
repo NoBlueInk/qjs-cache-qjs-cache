3d1eb397149af2d59474dc7558d343ee49e936c8
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-09-01 10:13:17 +0000

    Even more tests

--- src/main/kotlin/com/example/routes/mapper/signupMapper/UserQuertParameterMapper.kt
+++ src/main/kotlin/com/example/routes/mapper/signupMapper/UserQuertParameterMapper.kt
@@ -2,4 +2,7 @@ package com.example.routes.mapper.signupMapper
 
 import com.example.data.request.auth.UserRegisterRequest
 import io.ktor.server.application.*
-import i
+import io.ktor.server.util.*
+import io.ktor.util.pipeline.*
+
+fun PipelineContext<*, ApplicationCa
