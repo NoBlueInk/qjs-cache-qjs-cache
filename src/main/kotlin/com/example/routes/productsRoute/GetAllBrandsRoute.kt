2a1d0af718fe277e67390de331fefb999d209ed7
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-09-22 15:42:18 +0000

    Update travis configuration

--- src/main/kotlin/com/example/routes/productsRoute/GetAllBrandsRoute.kt
+++ src/main/kotlin/com/example/routes/productsRoute/GetAllBrandsRoute.kt
@@ -3,4 +3,8 @@ package com.example.routes.productsRoute
 import com.example.data.repositories.productRepository.ProductRepositoryImpl
 import com.example.domin.endPoints.ProductsEndPoint
 import com.example.utils.notFoundResponse
-import io.ktor.server.applica
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+f
