f45d1b0e6ba790b311a43ae3933209f11da277eb
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-09-23 15:54:22 +0000

    remove debugging

--- src/main/kotlin/com/example/routes/productsRoute/GetAllBrandsRoute.kt
+++ src/main/kotlin/com/example/routes/productsRoute/GetAllBrandsRoute.kt
@@ -7,4 +7,6 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-f
+fun Route.getAllBrandsRoute() {
+
+    val productRepository by lazy { ProductRepositoryIm
