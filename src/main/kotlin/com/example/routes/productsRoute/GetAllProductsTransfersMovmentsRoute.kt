d42e86856beee497750959d91497e174add83222
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-10-03 09:59:39 +0000

    Add same website.

--- src/main/kotlin/com/example/routes/productsRoute/GetAllProductsTransfersMovmentsRoute.kt
+++ src/main/kotlin/com/example/routes/productsRoute/GetAllProductsTransfersMovmentsRoute.kt
@@ -7,4 +7,5 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route
+fun Route.getAllProductTransfersMovementRoute() {
+    val productRepository by lazy
