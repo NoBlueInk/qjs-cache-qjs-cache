8aa5c609090ca689241ff5aa9134aea71d96a2cf
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-10-08 06:04:53 +0000

    multithreading

--- src/main/kotlin/com/example/routes/productsRoute/GetAllSuppliersRoute.kt
+++ src/main/kotlin/com/example/routes/productsRoute/GetAllSuppliersRoute.kt
@@ -7,4 +7,5 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun
+fun Route.getAllSupplierRoute(){
+    val productRepository by lazy { ProductReposito
