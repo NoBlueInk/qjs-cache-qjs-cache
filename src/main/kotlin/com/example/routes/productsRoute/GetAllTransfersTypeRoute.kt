8e04195db1368cd5fa71bcc9b9d082ec11d7b52d
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-10-08 12:36:35 +0000

    Développeur passionné

--- src/main/kotlin/com/example/routes/productsRoute/GetAllTransfersTypeRoute.kt
+++ src/main/kotlin/com/example/routes/productsRoute/GetAllTransfersTypeRoute.kt
@@ -1,2 +1,19 @@
 
 package com.example.routes.productsRoute
+
+import com.example.data.repositories.productRepository.ProductRepositoryImpl
+import com.example.domin.endPoints.ProductsEndPoint
+import com.example.utils.notFoundResponse
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.getAllTransfersTypeRoute() {
+    val productRepository by lazy { ProductRepositoryImpl() }
+    post(ProductsEndPoint.AllTransfersTypeEndPoint.path) {
+        val transfersType = productRepository.getAllTransfersType()
+        if (transfersType.isEmpty())
+            call.notFoundResponse()
+        else call.respond(transfersType)
+    }
+}
