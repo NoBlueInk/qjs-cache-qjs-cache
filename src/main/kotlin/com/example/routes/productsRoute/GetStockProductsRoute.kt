a2d87750f6e259bc9f92666fbbcd40e8dd742d54
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-10-28 10:21:17 +0000

    bridge: Correctly clamp MAX forward_delay when enabling STP

--- src/main/kotlin/com/example/routes/productsRoute/GetStockProductsRoute.kt
+++ src/main/kotlin/com/example/routes/productsRoute/GetStockProductsRoute.kt
@@ -8,4 +8,5 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.getAllProductsFromStockRoute() {
-    val productRepository b
+    val productRepository by lazy { ProductRepositoryImpl() }
+    post(ProductsEndPoint.AllProductFromStockEndPoint
