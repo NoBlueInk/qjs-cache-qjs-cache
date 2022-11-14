98a3e9187ecfcfb6c3ad08657bcf01333f7fdae3
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-11-14 15:38:36 +0000

    tweak travis

--- src/main/kotlin/com/example/routes/userRoute/DeleteUserRoute.kt
+++ src/main/kotlin/com/example/routes/userRoute/DeleteUserRoute.kt
@@ -11,4 +11,6 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Route
+fun Route.deleteUserRoute(
+    userDataSource: UserRepository,
+    hashingService: Hashi
