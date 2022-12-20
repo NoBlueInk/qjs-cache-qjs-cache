f1d85344d61d3149d1506d7528fa87d4671143e2
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-12-20 08:11:45 +0000

    Generated code from kurento-jsonrpc-js:c7b8522

--- src/main/kotlin/com/example/routes/userRoute/UpdatePasswordRoute.kt
+++ src/main/kotlin/com/example/routes/userRoute/UpdatePasswordRoute.kt
@@ -21,3 +21,80 @@ fun Route.updatePasswordRoute(
 //
 //        val areFieldsBlank = request.password.isBlank() || request.username.isBlank() || request.newPassword.isBlank()
 //        val isPwTooShort = request.password.length < 8 || request.newPassword.length < 8
+//
+//        if(areFieldsBlank || isPwTooShort) {
+//            call.respond(HttpStatusCode.Conflict)
+//            return@post
+//        }
+//
+//        val user = userDataSource.getUserByUserName(request.username)
+//
+//        if(user == null) {
+//            call.respond(
+//                HttpStatusCode.Conflict,
+//                message = AuthResponse(
+//                    success = false,
+//                    token = "",
+//                    message = "INCORRECT USERNAME OR PASSWORD"
+//                )
+//            )
+//            return@post
+//        }
+//
+//        val isValidPassword = hashingService.verify(
+//            value = request.password,
+//            saltedHash = SaltedHash(
+//                hash = user.userPassword,
+//                salt = user.userSalt
+//            )
+//        )
+//
+//        if(!isValidPassword) {
+//            println("Entered hash: ${DigestUtils.sha256Hex("${user.userSalt}${request.password}")}, Hashed PW: ${user.userPassword}")
+//            call.respond(
+//                HttpStatusCode.Conflict,
+//                message = AuthResponse(
+//                    success = false,
+//                    token = "",
+//                    message = "INCORRECT PASSWORD"
+//                )
+//            )
+//            return@post
+//        }
+//
+//        val saltedHash = hashingService.generateSaltedHash(value = request.newPassword)
+//
+//        val userCopy = user.copy(
+//            userPassword = saltedHash.hash,
+//            userSalt = saltedHash.salt
+//        )
+//
+//        val updateUser = userDataSource.updateUserPassword(userCopy)
+//
+//        if(!updateUser)
+//        {
+//            call.respond(
+//                status = HttpStatusCode.Conflict,
+//                message = AuthResponse(
+//                    success = false,
+//                    token = "",
+//                    message = "FAILED UPDATE USER"
+//                )
+//            )
+//            return@post
+//        }
+//
+//        val token = tokenService.generate(
+//            config = tokenConfig,
+//            Token(
+//                name = "userId",
+//                value = user.userId.toString()
+//            )
+//        )
+//
+//        call.respond(
+//            status = HttpStatusCode.OK,
+//            message = AuthResponse(
+//                success = true,
+//                token = token,
+//                message = "SUCCESS UPDATE USER"
