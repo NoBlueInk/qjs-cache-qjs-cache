dee9560497726784fdb8c9b30e817bf987ed28ad
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2022-12-28 20:49:51 +0000

    removed pyc

--- src/main/kotlin/com/example/security/HashingService.kt
+++ src/main/kotlin/com/example/security/HashingService.kt
@@ -3,4 +3,5 @@ package com.example.security
 interface HashingService {
     fun generateSaltedHash(value: String, saltedHash: Int = 32): SaltedHash
 
-    fun verify(value:String, saltedHash: 
+    fun verify(value:String, saltedHash: SaltedHash):Boolean
+}
