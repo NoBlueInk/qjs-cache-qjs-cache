4df8f98ffbb62481bd7061a29475b5331c871b09
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2023-01-02 17:20:49 +0000

    usb: gadget: midi: free hs descriptors

--- src/main/kotlin/com/example/security/SHA256HashingServiceImpl.kt
+++ src/main/kotlin/com/example/security/SHA256HashingServiceImpl.kt
@@ -3,3 +3,7 @@ package com.example.security
 import org.apache.commons.codec.binary.Hex
 import org.apache.commons.codec.digest.DigestUtils
 import java.security.SecureRandom
+
+class SHA256HashingServiceImpl : HashingService {
+
+    override fun generat
