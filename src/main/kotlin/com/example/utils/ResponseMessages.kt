f13a9d36600eddd11b10014a01d4c8920a8e93c9
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2023-02-14 15:51:40 +0000

    [maven-release-plugin] prepare release 0.6.0

--- src/main/kotlin/com/example/utils/ResponseMessages.kt
+++ src/main/kotlin/com/example/utils/ResponseMessages.kt
@@ -4,4 +4,6 @@ sealed class ResponseMessages(val message: String) {
     object IncorrectPasswordORUserName : ResponseMessages("INCORRECT USERNAME OR PASSWORD")
     object EmptyField : ResponseMessages("EMPTY FILED")
     object IncorrectPassword : ResponseMessages("INCORRECT PASSWORD")
-    object SuccessSignIn : ResponseMessages("SUCCESS SIGN
+    object SuccessSignIn : ResponseMessages("SUCCESS SIGN IN")
+    object IncorrectRequest : ResponseMessages("INCORRECT REQUEST")
+    object U
