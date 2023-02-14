77731730b579b6fd26c8c2cca304efd22b8129c0
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2023-02-14 06:00:31 +0000

    Update CI settings

--- src/main/kotlin/com/example/utils/ResponseMessages.kt
+++ src/main/kotlin/com/example/utils/ResponseMessages.kt
@@ -2,4 +2,5 @@ package com.example.utils
 
 sealed class ResponseMessages(val message: String) {
     object IncorrectPasswordORUserName : ResponseMessages("INCORRECT USERNAME OR PASSWORD")
-    object Em
+    object EmptyField : ResponseMessages("EMPTY FILED")
+    object IncorrectPassword : ResponseMes
