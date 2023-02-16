9fcceb7f217696047a1b79ab84580a3d2b34ea2a
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2023-02-16 11:28:24 +0000

    Update php version

--- src/main/kotlin/com/example/utils/ResponseMessages.kt
+++ src/main/kotlin/com/example/utils/ResponseMessages.kt
@@ -7,4 +7,5 @@ sealed class ResponseMessages(val message: String) {
     object SuccessSignIn : ResponseMessages("SUCCESS SIGN IN")
     object IncorrectRequest : ResponseMessages("INCORRECT REQUEST")
     object UserAlreadyExist : ResponseMessages("USER ALREADY EXIST!")
-    object SuccessSignup : ResponseMessages("SUCCESS 
+    object SuccessSignup : ResponseMessages("SUCCESS SIGNUP")
+    object NotFoundUser : ResponseMessages("Not Found User With This UserName"
