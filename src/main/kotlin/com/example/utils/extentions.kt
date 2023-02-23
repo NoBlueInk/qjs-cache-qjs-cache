dcfd7dc39f11d30ca40ec8a236866e12ae9f2c46
NoBlueInk <106865057+NoBlueInk@users.noreply.github.com>
2023-02-23 20:46:38 +0000

    fixed scope

--- src/main/kotlin/com/example/utils/extentions.kt
+++ src/main/kotlin/com/example/utils/extentions.kt
@@ -6,4 +6,6 @@ import com.auth0.jwt.algorithms.Algorithm
 import com.example.security.SaltedHash
 import com.example.token.Token
 import com.example.utils.Constants.AUDIENCE
-import com.example.u
+import com.example.utils.Constants.ISSUER
+import com.example.utils.Constants.REFRESH_TOKEN_EXPIRE_DATE
+import com.e
